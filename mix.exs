9d3d66cfa66730161fcc1c633fcba05d551e1f72
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2024-02-26 02:12:42 +0100

    chore: add package field

--- mix.exs
+++ mix.exs
@@ -5,25 +5,32 @@ defmodule StrapiMapper.MixProject do
     [
       app: :strapi_mapper,
       version: "0.1.0",
+      description:
+        "StrapiMapper is a simple Elixir library used to map Strapi responses to Elixir maps.",
       elixir: "~> 1.16",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
+      package: package(),
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+  defp package do
+    [
+      licenses: ["MIT"],
+      source_url: "https://github.com/zvonimirr/strapi_mapper"
+    ]
+  end
+
   defp deps do
     [
       {:ex_doc, "~> 0.31", only: :dev, runtime: false}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 end
