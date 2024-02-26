c017498abafbee5d41d043aba5ae22855db6cc8c
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2024-02-26 01:53:11 +0100

    feat: implement the mapper

--- lib/strapi_mapper.ex
+++ lib/strapi_mapper.ex
@@ -1,18 +1,35 @@
 defmodule StrapiMapper do
   @moduledoc """
-  Documentation for `StrapiMapper`.
+  StrapiMapper is a library for mapping Strapi responses to Elixir maps.
+  It doesn't care which JSON library you use, it just needs a map as input.
   """
 
   @doc """
-  Hello world.
+  Maps a Strapi response map to a normalized Elixir map.
 
-  ## Examples
+  If the input data is a list, it will map each element of the list.
 
-      iex> StrapiMapper.hello()
-      :world
+  If the input data is a map, it will map each key-value pair of the map.
 
+  Otherwise, it will return the input data.
   """
-  def hello do
-    :world
+  def map_to_struct(%{"data" => data = %{"attributes" => attributes}}) do
+    Enum.reduce(attributes, %{}, fn {key, value}, acc ->
+      Map.put(acc, key, map_to_struct(value))
+    end)
+    |> Map.put("id", data["id"])
   end
+
+  def map_to_struct(%{"data" => data}) do
+    Enum.map(data, &map_to_struct/1)
+  end
+
+  def map_to_struct(data = %{"attributes" => attributes}) do
+    Enum.reduce(attributes, %{}, fn {key, value}, acc ->
+      Map.put(acc, key, map_to_struct(value))
+    end)
+    |> Map.put("id", data["id"])
+  end
+
+  def map_to_struct(data), do: data
 end
