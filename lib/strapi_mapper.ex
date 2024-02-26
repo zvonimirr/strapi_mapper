00e0751eff6c14affa7de805b1325c71a6c24ede
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2024-02-26 01:47:06 +0100

    feat: initial commit

---
+++ lib/strapi_mapper.ex
@@ -0,0 +1,18 @@
+defmodule StrapiMapper do
+  @moduledoc """
+  Documentation for `StrapiMapper`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> StrapiMapper.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/strapi_mapper_test.exs
@@ -0,0 +1,8 @@
+defmodule StrapiMapperTest do
+  use ExUnit.Case
+  doctest StrapiMapper
+
+  test "greets the world" do
+    assert StrapiMapper.hello() == :world
+  end
+end
